4eee98d85077aad44a3e8a3af74f31782c1d2648
oyvind1501 <35425423+oyvind1501@users.noreply.github.com>
2018-04-13 19:32:15 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,15 +9,7 @@ func main() {
 	// Disse kan legges et annet sted slik at vi kun har go routines her
 	elevio.Init("localhost:15657", 4)
 	elev.TargetFloor = elev.UNDEFINED_TARGET_FLOOR
-	motorChannel := make(chan elev.MotorDirection)
-	lightChannel := make(chan elev.Light)
-	doorChannel := make(chan bool)
-	floorChannel := make(chan int)
-	buttonChannel := make(chan elevio.ButtonEvent)
-	requestChannel := make(chan elev.Action)
-	sendOrderChannel := make(chan elev.ElevatorOrderMessage)
-	receiveOrderChannel := make(chan elev.ElevatorOrderMessage)
-
+	
 	go elev.ActionController(buttonChannel, lightChannel, doorChannel, requestChannel, sendOrderChannel)
 	go elev.FiniteStateMachine(motorChannel, lightChannel, floorChannel, doorChannel, requestChannel)
 	go elev.FreeLockedOrders()
